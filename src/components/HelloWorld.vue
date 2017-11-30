a71183a7d946a6cef5f60e1fae0a3bb5d7bfb0d3
Peter <lawsonpd@gmail.com>
2017-11-30 14:55:45 -0600

    Edit npm start script. Change content of main page. Add server.js.

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
+    <router-link to="fraze">Fraze</router-link>
+    <!-- <h2>Essential Links</h2>
     <ul>
       <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
       <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
@@ -16,8 +17,7 @@
       <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
       <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
-    <router-link to="fraze">Fraze</router-link>
+    </ul> -->
   </div>
 </template>
 
@@ -26,7 +26,7 @@ export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'FRAZE'
     }
   }
 }
